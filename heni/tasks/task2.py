67b45b666df714833eefbceb4692f6990be6c15d
nguffanti <nguffanti@mercuriopi.com>
2023-02-06 01:07:12 -0300

    not friendly fix for task2

--- heni/tasks/task2.py
+++ heni/tasks/task2.py
@@ -4,9 +4,12 @@ import re
 dim_df = pd.read_csv("heni/resources/dim_df_correct.csv")
 result = []
 for index, row in dim_df.iterrows():
-    print(row['rawDim'].lower().replace("×","x"))
-    dims = re.findall(r'(\d+(?:[,\,.]\d+)?)\s*x\s*(\d+(?:[,\,.]\d+)?)(?:\s*x\s*(\d+(?:[,\,.]\d+)?))?',row["rawDim"].lower().replace("×","x").replace("by","x"))
-    result.append(dims[0])
+    if 'by' not in row["rawDim"]:
+        dims = re.findall(r'(\d+(?:[,\,.]\d+)?)\s*x\s*(\d+(?:[,\,.]\d+)?)(?:\s*x\s*(\d+(?:[,\,.]\d+)?))?\s*[cm]\s*',row["rawDim"].lower().replace("×","x").replace("by","x"))
+        result.append(dims[len(dims)-1])
+    else:
+        dims = re.findall(r'(\d+(?:[,\,.]\d+)?)\s*by\s*(\d+(?:[,\,.]\d+)?)(?:\s*by\s*(\d+(?:[,\,.]\d+)?))?',row["rawDim"].lower())
+        result.append((float(dims[0][0])*2.54,float(dims[0][1])*2.54,''))
 
 x = pd.DataFrame(result, columns=['height','width','depth'])
 x.to_csv("output_task2.csv", index=False)
